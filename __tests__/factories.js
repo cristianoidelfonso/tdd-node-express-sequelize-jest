5e810c114c18544486f092d6e64da56cb94ec2fa
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-09-28 17:32:36 -0300

    Implementando autenticação com jwt e middlewares

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -1,11 +1,11 @@
-// const faker = require("faker");
-const { faker } = require('@faker-js/faker');
+// const { faker } = require('@faker-js/faker');
+const faker = require("faker");
 const { factory } = require("factory-girl");
 const { User } = require("../src/app/models");
 
 factory.define("User", User, {
-  // name: faker.name.findName(),
-  name: faker.name.fullName(),
+  // name: faker.name.fullName(),
+  name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password()
 });
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 const routes = require('express').Router();
+const authMiddleware = require("./app/middlewares/auth");
 const SessionController = require('./app/controllers/SessionController');
 
 // Definição das rotas
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/dashboard', (req, res) => {
+  return res.status(200).send();
+});
 
 module.exports = routes;
